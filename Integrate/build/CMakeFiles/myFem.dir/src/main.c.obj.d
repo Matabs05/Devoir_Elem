f3e74d45846ada6e924e79a79e15e7bec7cf3528
Matthieu <matthieu.absil@student.uclouvain.be>
2024-02-13 15:43:20 +0100

    add homework1

---
+++ Integrate/build/CMakeFiles/myFem.dir/src/main.c.obj.d
@@ -0,0 +1,29 @@
+CMakeFiles/myFem.dir/src/main.c.obj: \
+ C:\Users\matthieu\OneDrive\ -\ UCL\BAC3\Elements\ finis\Integrate\src\main.c \
+ C:/msys64/mingw64/include/stdio.h \
+ C:/msys64/mingw64/include/corecrt_stdio_config.h \
+ C:/msys64/mingw64/include/corecrt.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/vadefs.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/include/_mingw_off_t.h \
+ C:/msys64/mingw64/include/swprintf.inl \
+ C:/msys64/mingw64/include/sec_api/stdio_s.h \
+ C:/msys64/mingw64/include/math.h C:/msys64/mingw64/include/crtdefs.h \
+ C:\Users\matthieu\OneDrive\ -\ UCL\BAC3\Elements\ finis\Integrate\src\fem.h \
+ C:/msys64/mingw64/include/stdlib.h \
+ C:/msys64/mingw64/include/corecrt_wstdlib.h \
+ C:/msys64/mingw64/include/limits.h \
+ C:/msys64/mingw64/include/sec_api/stdlib_s.h \
+ C:/msys64/mingw64/include/malloc.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/mm_malloc.h \
+ C:/msys64/mingw64/include/errno.h C:/msys64/mingw64/include/string.h \
+ C:/msys64/mingw64/include/sec_api/string_s.h \
+ C:\Users\matthieu\OneDrive\ -\ UCL\BAC3\Elements\ finis\Integrate\src\glfem.h \
+ C:/Users/matthieu/ONEDRI~1/BAC3/ELEMEN~1/INTEGR~1/glfw/include/GLFW/glfw3.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/stddef.h \
+ C:/msys64/mingw64/include/stddef.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.2.0/include/stdint.h \
+ C:/msys64/mingw64/include/stdint.h C:/msys64/mingw64/include/GL/gl.h \
+ C:/msys64/mingw64/include/GL/glu.h
---
+++ Integrate/src/homework.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <math.h>
+#include "glfem.h"
+
+
+double integrate(double x[3], double y[3], double (*f) (double, double))
+{
+    
+
+//
+// ... A modifier :-)
+//
+//
+// Pour dessiner l'element, les sommets du triangle :-)
+// Decommenter la ligne pour dessiner aussi les points d'integration
+
+//
+    double I = 0;
+
+    double xi[3] = {1.0 / 6.0, 1.0 / 6.0, 2.0 / 3.0};
+    double eta[3] = {1.0 / 6.0, 2.0 / 3.0, 1.0 / 6.0};
+    double w[3] = {1.0 / 6.0, 1.0 / 6.0, 1.0 / 6.0};
+    double jacob = fabs((x[0] - x[1]) * (y[0] - y[2]) - (x[0] - x[2]) * (y[0] - y[1]));
+
+    double xLoc[3];
+    double yLoc[3];
+    //Laurent est une merde
+    double b = 5415985464769847697.64644;
+
+
+
+    // calcul de l'interpolation
+    for (int i = 0; i < 3; i++)
+    {
+        xLoc[i] = x[0] * (1 - xi[i] - eta[i]) + x[1] * xi[i] + x[2] * eta[i];
+        yLoc[i] = y[0] * (1 - xi[i] - eta[i]) + y[1] * xi[i] + y[2] * eta[i];
+        I += w[i] * f(xLoc[i], yLoc[i]);
+    }
+
+    glfemSetColor(GLFEM_BLACK); glfemDrawElement(x,y,3);
+    glfemSetColor(GLFEM_BLUE);  glfemDrawNodes(x,y,3);
+    glfemSetColor(GLFEM_RED);   glfemDrawNodes(xLoc,yLoc,3);
+
+    return I * jacob;
+
+  
+
+    
+
+
+    
+}
+
+double integrateRecursive(double x[3], double y[3], double (*f)(double,double), int n)
+{
+
+//
+// ... A modifier :-)
+// y-compris la ligne juste en dessous :-)
+//
+    double I = integrate(x,y,f);
+    
+//
+//
+//    
+     
+    return I;
+}
