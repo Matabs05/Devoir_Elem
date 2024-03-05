51c0a5b4c61ff35d0d4c191bd271ec656dc95627
laurent0404 <laurent.jacquerye@student.uclouvain.be>
2024-03-05 15:44:28 +0100

    pushhhhhhhh siiii

--- BandSolver/src/homework.c
+++ BandSolver/src/homework.c
@@ -47,6 +47,12 @@ int femMeshComputeBand(femMesh *theMesh)
 void femBandSystemAssemble(femBandSystem* myBandSystem, double *Aloc, double *Bloc, int *map, int nLoc)
 {
     // A Ecrire :-)
+    int i,j;
+    for (i = 0; i < nLoc; i++) { 
+        for(j = 0; j < nLoc; j++) {
+            if(map[i] <= map[j] && map[j] <= map[i] + myBandSystem->band)
+                myBandSystem->A[map[i]][map[j]] += Aloc[i*nLoc+j]; } 
+    myBandSystem->B[map[i]] += Bloc[i]; }
 }
 
 
@@ -63,8 +69,27 @@ double  *femBandSystemEliminate(femBandSystem *myBand)
     size = myBand->size;
     band = myBand->band;
     
-    // A completer :-)
+    //à compléter
+    for (k=0; k < size; k++) {
+        jend = (k+band < size) ? k+band : size;
+        if ( fabs(A[k][k]) <= 1e-8 ) {
+            printf("Pivot index %d  ",k);
+            printf("Pivot value %e  ",A[k][k]);
+            Error("Cannot eliminate with such a pivot"); }
+        for (i = k+1 ; i <  jend; i++) {
+            factor = A[i][k] / A[k][k];
+            for (j = k+1 ; j < jend; j++) 
+                A[i][j] = A[i][j] - A[k][j] * factor;
+            B[i] = B[i] - B[k] * factor; }}
+    
+    /* Back-substitution */
 
+    for (i = size-1; i >= 0 ; i--) {
+        factor = 0;
+        jend = (i+band < size) ? i+band : size;
+        for (j = i+1 ; j < jend; j++)
+            factor += A[i][j] * B[j];
+        B[i] = ( B[i] - factor)/A[i][i]; }
 
     return(myBand->B);
 }
